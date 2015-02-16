eec3667618c81472ccacd7b6dbedd50f53a8c2e2
NaoyaTakamatsu <naoya.takamatsu@gmail.com>
2015-02-17 06:32:22 +0900

    Facebookログインボタン編集&申請ボタンの編集

--- js/o-tea.js
+++ js/o-tea.js
@@ -32,17 +32,96 @@ jQuery(function ($){
 	});
 });
 //申請ボタンの処理
-$(document).ready( function() {
+/*$(document).ready( function() {
 	//初期状態は0申請無し
 	var sinsei_jyoutai=0;
         $("#sinsei_button").click(function(){
-            alert('申請します!!');
+            //alert('申請します!!');
             //申請済みは1
+            $("#sinsei_condition").html("さんの申請状態を選択してください");
+            $(".friend_list").html("<p id="sinseizumi">申請済み</p>");
 	var sinsei_jyoutai=1;
         });
         $("#torikeshi_button").click(function(){
-            alert('取り消します!!');
+            //alert('取り消します!!');
             //申請済みは0
+            $("#sinsei_condition").html("さんの申請状態を選択してください");
 	var sinsei_jyoutai=0;
         });
-    });
+        $("#closed").click(function(){
+            //alert('閉じます');
+            //申請済みは0
+	//var sinsei_jyoutai=0;
+	$("#sinsei_condition").html("さんの申請状態を選択してください");
+        });
+    });*/
+
+//申請ボタンの処理
+$(function() {
+$(".friend_list").click(function () {
+//申請時のダイアログ表示時の名前入れの処理
+  var $name=$(this).find('h3').text();
+  var $condition=$(this).find('p');
+  //var $condition_id=$(this).find('p id');
+  var $idname = $(this).find('p').attr("id"); 
+  $("#sinsei_condition").prepend($name);
+ 
+  var sinsei_jyoutai=0;
+  //申請ボタン
+        $("#sinsei_button").click(function(){
+            //alert($idname);
+            //申請済みは1
+            $("#sinsei_condition").html("さんの申請状態を選択してください");
+            $condition.html('<p id="sinseizumi" class="ui-li-desc">申請済み</p>');
+            //$idname="sinseizumi";
+            /*if($idname=="ryouomoi"){
+            	//$idname="ryouomoi";
+            	$(this).find('p').html('<p id="ryouomoi">両想い</p>');
+            	//$condition.html('<p id="ryouomoi">両想い</p>');
+            }
+            else if($idname=="sinseimae"||"sinseizumi"){
+            	$(this).find('p').html('<p id="sinseizumi">申請済み</p>');
+            	//$condition.html('<p id="sinseizumi">申請済み</p>');
+            }*/
+            /*if($idname=="sinseimae"){
+		$condition.html('<p id="sinseizumi">申請済み</p>');
+		//$idname="sinseizumi";
+            }*/
+            /*else if($idname=="sinseizumi"){
+            	//$idname="sinseizumi";
+
+            }
+            else if($idname=="ryouomoi"){
+            	//$idname="ryouomoi";
+            }*/
+            //$(".friend_list").html("<p id="sinseizumi">申請済み</p>");
+	var sinsei_jyoutai=1;
+        });
+    //取り消しボタン
+        $("#torikeshi_button").click(function(){
+            //alert('取り消します!!');
+            //申請済みは0
+            $("#sinsei_condition").html("さんの申請状態を選択してください");
+            $condition.html('<p id="sinseimae">申請前</p>');
+            /*if($idname=="sinseizumi"){
+		$condition.html('<p id="sinseimae">申請前</p>');
+		//$idname="sinseimae";
+            }
+            else if($idname=="ryouomoi"){
+            	$condition.html('<p id="sinseimae">申請前</p>');
+            	//$idname="sinseimae";
+            }*/
+            /*else if($idname=="sinseimae"){
+            	//$idname="sinseimae";
+            }*/
+	var sinsei_jyoutai=0;
+        });
+     //閉じるボタン
+        $("#closed").click(function(){
+            //alert('閉じます');
+            //申請済みは0
+	//var sinsei_jyoutai=0;
+	$("#sinsei_condition").html("さんの申請状態を選択してください");
+        });
+});
+});
